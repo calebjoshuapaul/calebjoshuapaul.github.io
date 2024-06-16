8c03a9ef3a5d47c254652d4478e9e9b56bc8b13e
Caleb Joshua <calebjoshuapaul@gmail.com>
2024-06-16 15:32:16 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/calebjoshuapaul.github.io/",
+	base: "./",
 	plugins: [react()],
 	assetsInclude: ["**/*.glb"],
 });
